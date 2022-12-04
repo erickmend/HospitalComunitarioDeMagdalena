1236b906372d37e4851138ce1a7dad7763c31f28
erickmen <erickmendoza@xipetechnology.com>
2022-12-04 14:04:31 -0700

    [Dev] EquiposCRUD

---
+++ WebApp/Data/IJsExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Data
+{
+    public static class IJsExtensions
+    {
+        public static async Task<object> ShowAlert(this IJSRuntime js, string title, string message)
+        {
+            return await js.InvokeAsync<object>("swal", new
+            {
+                title = title,
+                text = message,
+                confirmButtonColor = "#002E6D",
+                confirmButtonText = "Ok",
+                width = 630,
+                padding = 80,
+                position = "center"
+            });
+        }
+
+        public static async Task<object> QuestionAlert(this IJSRuntime js, string title, string message )
+        {
+            return await js.InvokeAsync<object>("swal", new
+            {
+                title = title,
+                text = message,
+                buttons = true,
+                showCancelButton = true,
+                confirmButtonColor = "#38bd60",
+                confirmButtonText = "Yes",
+                cancelButtonColor = "#ff5352",
+                cancelButtonText = "Cancel",
+                width = 630,
+                padding = 80
+            });
+        }
+    }
+}
